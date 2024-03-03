280fa561f1e25a274967880fdac8d2fe39df73c7
Varga György Márk <gyorgy.varga@shiwaforce.com>
2024-03-03 20:14:44 +0100

    Added database access, authentication and login page

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  pages: {
+    signIn: '/auth/signin',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? '',
+      clientSecret: process.env.GOOGLE_SECRET ?? '',
+    }),
+  ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
