2556031ebcd09d632ba53117077fef9abed53c18
Varga György Márk <gyorgy.varga@shiwaforce.com>
2024-03-31 21:03:53 +0200

    displaying the pokemons in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '@/utils/authOptions';
+import { Pokemon } from '@prisma/client';
+import { db } from '@/prisma';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -9,9 +11,24 @@ export default async function Home() {
     redirect('/auth/signin');
   }
 
+  const pokemons = await db.pokemon.findMany({
+    take: 2,
+    orderBy: {
+      id: 'asc',
+    },
+  });
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>This is the Pokemon Voting App Only for logged in users</h1>
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <h1>Which Pokémon Do You Like More?</h1>
+      <div className='flex'>
+        {pokemons.map((pokemon: Pokemon) => (
+          <div key={pokemon.id} className='pokemon'>
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.image} alt={pokemon.name} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,47 +1,45 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { db } from '@/prisma/index';
 
 async function main() {
-    const basicPokemonData = await fetchBasicPokemonData();
-    for (const basicData of basicPokemonData) {
-        const detailedData = await fetchPokemonDetails(basicData.url);
-        await prisma.pokemon.create({
-            data: {
-                name: detailedData.name,
-                image: detailedData.sprites.front_default
-            },
-        });
-    }
+  const basicPokemonData = await fetchBasicPokemonData();
+  for (const basicData of basicPokemonData) {
+    const detailedData = await fetchPokemonDetails(basicData.url);
+    await db.pokemon.create({
+      data: {
+        name: detailedData.name,
+        image: detailedData.sprites.front_default,
+      },
+    });
+  }
 }
 
 async function fetchBasicPokemonData() {
-    const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100';
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        console.error('Failed to fetch basic Pokemon data:', error);
-        return [];
-    }
+  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100';
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error('Failed to fetch basic Pokemon data:', error);
+    return [];
+  }
 }
 
 async function fetchPokemonDetails(url: string) {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.error(`Failed to fetch Pokemon details from ${url}:`, error);
-        throw error;
-    }
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(`Failed to fetch Pokemon details from ${url}:`, error);
+    throw error;
+  }
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
