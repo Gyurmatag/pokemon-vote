e91dc79ed76b5a1cdad69c9382d8e8e935e8d85b
Varga György Márk <gyorgy.varga@shiwaforce.com>
2024-04-01 11:01:30 +0200

    added the top 10 pokemon display

--- app/top-10/page.tsx
+++ app/top-10/page.tsx
@@ -1,9 +1,36 @@
-import { Pokemon } from '@prisma/client';
+import { db } from '@/prisma';
 
 export default async function Top10() {
+  const topPokemons = await db.pokemon.findMany({
+    take: 10,
+    where: {
+      votes: {
+        some: {},
+      },
+    },
+    orderBy: {
+      votes: {
+        _count: 'desc',
+      },
+    },
+    include: {
+      votes: {
+        select: { id: true },
+      },
+    },
+  });
+
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <h1>The Top 10 Pokémon</h1>
+      <ul>
+        {topPokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h2>{pokemon.name}</h2>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
