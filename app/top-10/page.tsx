6277f1e2d9f389f8910baaeca0b55d39433456c8
Varga György Márk <gyorgy.varga@shiwaforce.com>
2024-04-01 19:24:51 +0200

    added skeleton loaders

--- app/top-10/page.tsx
+++ app/top-10/page.tsx
@@ -1,8 +1,9 @@
-import { db } from '@/prisma';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/authOptions';
 import { redirect } from 'next/navigation';
-import Image from 'next/image';
+import List from '@/app/top-10/List';
+import { Suspense } from 'react';
+import { Top10PokemonSkeletonLoader } from '@/components/skeleton-loaders/Top10PokemonSkeletonLoader';
 
 export default async function Top10() {
   const session = await getServerSession(authOptions);
@@ -11,50 +12,14 @@ export default async function Top10() {
     redirect('/auth/signin');
   }
 
-  const topPokemons = await db.pokemon.findMany({
-    take: 10,
-    where: {
-      votes: {
-        some: {},
-      },
-    },
-    orderBy: {
-      votes: {
-        _count: 'desc',
-      },
-    },
-    include: {
-      votes: {
-        select: { id: true },
-      },
-    },
-  });
-
   return (
     <main className='flex min-h-screen flex-col items-center px-4 py-10 md:px-24'>
       <h1 className='mb-6 text-2xl font-semibold md:text-3xl'>
         The Top 10 Pokémon
       </h1>
-      <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {topPokemons.map((pokemon) => (
-          <div
-            key={pokemon.id}
-            className='flex flex-col items-center rounded-lg border bg-white p-4 shadow-sm'
-          >
-            <Image
-              width={100}
-              height={100}
-              src={pokemon.image}
-              alt={pokemon.name}
-              className='mb-2'
-            />
-            <h2 className='text-lg font-medium'>{pokemon.name}</h2>
-            <span className='text-sm text-gray-600'>
-              Votes: {pokemon.votes.length}
-            </span>
-          </div>
-        ))}
-      </div>
+      <Suspense fallback={<Top10PokemonSkeletonLoader />}>
+        <List />
+      </Suspense>
     </main>
   );
 }
---
+++ components/PokemonVoteForm.tsx
@@ -0,0 +1,135 @@
+import { redirect } from 'next/navigation';
+import { Form, Move, Pokemon, Stat } from '@prisma/client';
+import { db } from '@/prisma';
+import { voteOnPokemon } from '@/app/actions';
+import { revalidatePath } from 'next/cache';
+import Image from 'next/image';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Heart, Zap, Shield, Eye, Wind, Star } from 'lucide-react';
+
+export default async function PokemonVoteForm() {
+  const totalPokemon = await db.pokemon.count();
+  const randomOffset = Math.floor(Math.random() * (totalPokemon - 2));
+  const pokemons = await db.pokemon.findMany({
+    take: 2,
+    skip: randomOffset,
+    include: {
+      forms: true,
+      moves: true,
+      stats: true,
+    },
+  });
+
+  const vote = async (formData: FormData) => {
+    'use server';
+    const votedPokemonId = formData.get('vote');
+    if (votedPokemonId) {
+      await voteOnPokemon(formData);
+      revalidatePath('/');
+      redirect('/');
+    }
+  };
+
+  function IconForStat(name) {
+    switch (name.name) {
+      case 'hp':
+        return <Heart className='h-5 w-5 text-red-500' />;
+      case 'attack':
+        return <Zap className='h-5 w-5 text-yellow-500' />;
+      case 'defense':
+        return <Shield className='h-5 w-5 text-green-500' />;
+      case 'special-attack':
+        return <Eye className='h-5 w-5 text-blue-500' />;
+      case 'special-defense':
+        return <Star className='h-5 w-5 text-purple-500' />;
+      case 'speed':
+        return <Wind className='h-5 w-5 text-orange-500' />;
+      default:
+        return <Shield className='h-5 w-5 text-gray-500' />;
+    }
+  }
+
+  return (
+    <form action={vote} className='flex w-full flex-wrap justify-center'>
+      {pokemons.map(
+        (
+          pokemon: Pokemon & { forms: Form[]; moves: Move[]; stats: Stat[] }
+        ) => (
+          <div
+            key={pokemon.id}
+            className='m-4 flex h-full w-full flex-col rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 md:w-1/3'
+          >
+            <div className='flex flex-col items-center p-3 sm:p-4 md:p-6'>
+              <Image
+                width={100}
+                height={100}
+                src={pokemon.image}
+                alt={pokemon.name}
+                className='mb-2 h-24 w-24 rounded-full shadow-lg sm:h-28 sm:w-28 md:mb-3 md:h-28 md:w-28'
+              />
+              <h5 className='mb-1 text-lg font-medium text-gray-900 dark:text-white sm:text-xl md:mb-2'>
+                {pokemon.name}
+              </h5>
+              <span className='text-xs text-gray-500 dark:text-gray-400 sm:text-sm'>
+                Height: {pokemon.height} cm
+              </span>
+              <span className='text-xs text-gray-500 dark:text-gray-400 sm:text-sm'>
+                Weight: {pokemon.weight} kg
+              </span>
+              <Button
+                name='vote'
+                value={pokemon.id}
+                className='mt-3 rounded px-4 py-1 sm:mt-4 sm:px-6 sm:py-2'
+              >
+                Vote
+              </Button>
+            </div>
+            <div className='border-t border-gray-200 dark:border-gray-700'>
+              <h3 className='px-5 pb-2 pt-4 text-lg font-semibold text-gray-900 dark:text-white'>
+                Stats
+              </h3>
+              <ul className='grid grid-cols-2 gap-2 px-5 pb-4'>
+                {pokemon.stats.map((stat) => (
+                  <li
+                    key={stat.id}
+                    className='flex items-center text-sm text-gray-700 dark:text-gray-400'
+                  >
+                    <IconForStat name={stat.name} />
+                    <span className='ml-2'>
+                      {stat.name}: {stat.value}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <Accordion type='single' collapsible className='w-full'>
+              <AccordionItem value='moves'>
+                <AccordionTrigger className='bg-gray-100 px-5 py-4 text-left text-lg font-semibold text-gray-900 dark:bg-gray-700 dark:text-white'>
+                  Moves
+                </AccordionTrigger>
+                <AccordionContent className='px-5 py-4'>
+                  <ul>
+                    {pokemon.moves.map((move) => (
+                      <li
+                        key={move.id}
+                        className='text-sm text-gray-700 dark:text-gray-400'
+                      >
+                        {move.name}
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        )
+      )}
+    </form>
+  );
+}
